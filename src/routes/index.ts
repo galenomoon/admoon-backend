d7645244b62ecc4101157193c35826f343676ccc
galenomoon <galeno.dev@gmail.com>
2023-09-28 23:29:19 -0300

    routes of products now integrated with website

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import authRoutes from "./auth.routes";
 import adminRoutes from "./admins.routes";
 import imagesRoutes from "./images.routes";
 import websitesRoutes from "./websites.routes";
-import productsRoutes from "./products.routes";
 import servicesRoutes from "./services.routes";
 import superUserRoutes from "./superUsers.routes";
 
@@ -14,7 +13,6 @@ routes.use("/services", servicesRoutes);
 routes.use("/websites", websitesRoutes);
 routes.use("/admins", adminRoutes);
 routes.use("/superusers", superUserRoutes);
-routes.use("/products", productsRoutes);
 routes.use("/images", imagesRoutes);
 
 routes.get("/", (_, res) => res.status(200).json({ message: "OK!" }));
--- src/routes/websites.routes.ts
+++ src/routes/websites.routes.ts
@@ -1,28 +1,48 @@
 import { Router } from "express";
+
+//middlewares
 import { authenticateToken } from "../middlewares/authMiddleware";
+
+//controllers
 import WebsiteController from "../controllers/websitesController";
 import CategoryController from "../controllers/categoriesController";
+import ProductController from "../controllers/productsController";
 
 const websitesRoutes = Router();
 
 const websiteController = new WebsiteController();
+const productController = new ProductController();
 const categoryController = new CategoryController();
 
-//REST ROUTES
+//REST
 websitesRoutes.get("/", authenticateToken, websiteController.getAll);
 websitesRoutes.get("/:id", authenticateToken, websiteController.getById);
 websitesRoutes.post("/", authenticateToken, websiteController.create);
 websitesRoutes.put("/:id", authenticateToken, websiteController.update);
 websitesRoutes.delete("/:id", authenticateToken, websiteController.delete);
 
-//CUSTOM ROUTES
+//CUSTOM
 websitesRoutes.post("/:id/services", authenticateToken, websiteController.handleServices);
 
-//CATEGORIES ROUTES
+// ------------------ WEBSITE CATEGORIES ROUTES ------------------
+
+//REST
 websitesRoutes.get("/:websiteId/categories/", categoryController.getAll);
 websitesRoutes.get("/:websiteId/categories/:id", categoryController.getById);
 websitesRoutes.post("/:websiteId/categories/", authenticateToken, categoryController.create);
 websitesRoutes.put("/:websiteId/categories/:id", authenticateToken, categoryController.update);
 websitesRoutes.delete("/:websiteId/categories/:id", authenticateToken, categoryController.delete);
+
+// ------------------ WEBSITE PRODUCTS ROUTES ------------------
+
+//REST
+websitesRoutes.get("/:websiteId/products/", productController.getAll);
+websitesRoutes.get("/:websiteId/products/:idOrSlug", productController.getByIdOrSlug);
+websitesRoutes.post("/:websiteId/products/", authenticateToken, productController.create);
+websitesRoutes.put("/:websiteId/products/:id", authenticateToken, productController.update);
+websitesRoutes.delete("/:websiteId/products/:id", authenticateToken, productController.delete);
+
+//CUSTOM 
+websitesRoutes.get("/:websiteId/products/category/:categoryIdOrSlug", productController.getByCategory);
 
 export default websitesRoutes;
