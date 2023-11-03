803baee87a5008e2196440c4193e09b7016c0cb0
galenomoon <galeno.dev@gmail.com>
2023-11-02 23:51:38 -0300

    increased limit of images per request

--- src/routes/images.routes.ts
+++ src/routes/images.routes.ts
@@ -9,12 +9,12 @@ const imagesController = new ImagesController();
 //REST
 imagesRoutes.get("/", imagesController.getAll);
 imagesRoutes.get("/:id", imagesController.getById);
-imagesRoutes.post("/:productId", upload.array('images', 6), imagesController.create);
+imagesRoutes.post("/:productId", upload.array('images', 22), imagesController.create);
 imagesRoutes.delete("/:id", imagesController.delete);
 
 // CUSTOM ROUTES
 imagesRoutes.get("/product/:productId", imagesController.getByProductId);
 imagesRoutes.get("/product/slug/:productSlug", imagesController.getByProductSlug);
-imagesRoutes.post("/shuffle/:productId", upload.array('images', 6), imagesController.shuffleImages);
+imagesRoutes.post("/shuffle/:productId", upload.array('images', 22), imagesController.shuffleImages);
 
 export default imagesRoutes;
