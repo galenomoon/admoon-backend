bddb1acf0c56e7a4bb1b5955c24f451994cb88ee
galenomoon <galeno.dev@gmail.com>
2023-11-02 23:52:02 -0300

    comment not finished code

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -32,27 +32,27 @@ async function seed() {
     await prisma.category.deleteMany();
   }
 
-  async function ecommerce() {
-    console.log("E-commerce seeded.");
-    let newCategories: Promise<Category>[] = [];
-    categories.forEach((name) => {
-      const newCategory = categoryModel.create({ name });
-      newCategories.push(newCategory);
-    });
+  // async function ecommerce() {
+  //   console.log("E-commerce seeded.");
+  //   let newCategories: Promise<Category>[] = [];
+  //   categories.forEach((name) => {
+  //     const newCategory = categoryModel.create({ name });
+  //     newCategories.push(newCategory);
+  //   });
 
-    newCategories.forEach(async (category) => {
-      const currentCategory = await category;
-      adjectives.forEach(async (adjective) => {
-        await productModel.create({
-          name: `${currentCategory.name} ${adjective}`,
-          description: `Introduzindo o incrível produto ${adjective} ${currentCategory.name} - a escolha definitiva para quem busca a excelência. Com qualidade incomparável e desempenho excepcional, o ${currentCategory.name} é simplesmente o melhor em sua categoria.`,
-          price: Math.floor(Math.random() * 1000),
-          images: [""],
-          categoryId: Number(currentCategory.id),
-        });
-      });
-    });
-  }
+  //   newCategories.forEach(async (category) => {
+  //     const currentCategory = await category;
+  //     adjectives.forEach(async (adjective) => {
+  //       await productModel.create({
+  //         name: `${currentCategory.name} ${adjective}`,
+  //         description: `Introduzindo o incrível produto ${adjective} ${currentCategory.name} - a escolha definitiva para quem busca a excelência. Com qualidade incomparável e desempenho excepcional, o ${currentCategory.name} é simplesmente o melhor em sua categoria.`,
+  //         price: Math.floor(Math.random() * 1000),
+  //         images: [""],
+  //         categoryId: Number(currentCategory.id),
+  //       });
+  //     });
+  //   });
+  // }
 
   try {
     await cleanDB();
