d9f598a03a634960412ef305e1d6d7716980920b
galenomoon <galeno.dev@gmail.com>
2024-02-11 16:05:25 -0300

    Add Swagger authentication middleware

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -1,9 +1,25 @@
-import YAML from 'yamljs';
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
-const swaggerDocument = YAML.load('./src/config/swagger.yaml');
+import YAML from "yamljs";
+import express from "express";
+import basicAuth from "basic-auth";
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = YAML.load("./src/config/swagger.yaml");
 
-const docs = express()
-docs.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const docs = express();
 
-export default docs
+docs.use((req, res, next) => {
+  const credentials = basicAuth(req);
+
+  if (
+    !credentials ||
+    credentials.name !== process.env.SWAGGER_USERNAME ||
+    credentials.pass !== process.env.SWAGGER_PASSWORD
+  ) {
+    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+    return res.sendStatus(401);
+  }
+  next();
+});
+
+docs.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default docs;
