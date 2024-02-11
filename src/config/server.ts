d543cc239049f9d582fdc7e6568e1c0bc8b12a87
galenomoon <galeno.dev@gmail.com>
2024-02-11 16:13:36 -0300

    Update server configuration to use Swagger documentation

--- src/config/server.ts
+++ src/config/server.ts
@@ -2,15 +2,19 @@ import 'express-async-errors'
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import docs from './docs'
+import YAML from "yamljs";
 import routes from '../routes'
+import swaggerUi from "swagger-ui-express";
+const swaggerDocument = YAML.load("./src/config/swagger.yaml");
 import { errorHandler } from '../middlewares/errorHandler'
 
 const server = express()
 
 server.use(cors())
 server.use(express.json({ limit: '20mb' }))
-server.use(docs)
+
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 server.use(routes)
 server.use(errorHandler)
 
