e82b6155776b7a03356f45e3bcb1ed51068672bd
galenomoon <galeno.dev@gmail.com>
2024-02-11 16:10:32 -0300

    rmeoving auth processo to swagger

--- src/config/docs.ts
+++ src/config/docs.ts
@@ -6,19 +6,19 @@ const swaggerDocument = YAML.load("./src/config/swagger.yaml");
 
 const docs = express();
 
-docs.use((req, res, next) => {
-  const credentials = basicAuth(req);
+// docs.use((req, res, next) => {
+//   const credentials = basicAuth(req);
 
-  if (
-    !credentials ||
-    credentials.name !== process.env.SWAGGER_USERNAME ||
-    credentials.pass !== process.env.SWAGGER_PASSWORD
-  ) {
-    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
-    return res.sendStatus(401);
-  }
-  next();
-});
+//   if (
+//     !credentials ||
+//     credentials.name !== process.env.SWAGGER_USERNAME ||
+//     credentials.pass !== process.env.SWAGGER_PASSWORD
+//   ) {
+//     res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+//     return res.sendStatus(401);
+//   }
+//   next();
+// });
 
 docs.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
