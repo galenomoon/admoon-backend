eb94b8558ab167ff5b27e2ce01dbdd6168e89008
galenomoon <galeno.dev@gmail.com>
2024-02-12 00:03:05 -0300

    config swagger in vercel

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,16 +3,25 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import routes from '../routes'
+import bodyParser from 'body-parser'
 import swaggerUi from "swagger-ui-express";
-const swaggerDocument = require('./swagger.json');
 import { errorHandler } from '../middlewares/errorHandler'
 
 const server = express()
+const swaggerDocument = require('./swagger.json');
+const options = { 
+  customCss: `https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css`,
+  customSiteTitle: "Admoon API Docs - Swagger"
+}
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 server.use(cors())
 server.use(express.json({ limit: '20mb' }))
 
-server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options ));
 
 server.use(routes)
 server.use(errorHandler)
