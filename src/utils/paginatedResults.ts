0850b0faff47273a6a577962bb688b036f0095a9
galenomoon <galeno.dev@gmail.com>
2024-07-23 22:08:10 -0300

    parse api

--- src/utils/paginatedResults.ts
+++ src/utils/paginatedResults.ts
@@ -1,6 +1,6 @@
 export function paginatedResults(page = 1, data: [], perPage = 10) {
   const currentPage = page || 1;
-  const limit = !isNaN(Number(perPage)) && (perPage > 0) ? perPage : 10;
+  const limit = !isNaN(Number(perPage)) && (perPage > 0) ? Number(perPage) : 10;
   const startIndex = (currentPage - 1) * limit;
   const endIndex = currentPage * limit;
 
